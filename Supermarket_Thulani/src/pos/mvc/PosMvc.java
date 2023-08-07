03805be2c908ddbf41a101fc4fb7d8ea0fbd334a
Thulani Wijenayake <141403478+Thulaniiiiii@users.noreply.github.com>
2023-08-07 10:34:38 +0530

    Add files via upload

---
+++ Supermarket_Thulani/src/pos/mvc/PosMvc.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos.mvc;
+
+import pos.mvc.view.CustomerView;
+import pos.mvc.view.ItemView;
+import pos.mvc.view.OrderView;
+
+
+public class PosMvc {
+
+
+    public static void main(String[] args) {
+       // new CustomerView().setVisible(true);
+       new OrderView().setVisible(true);
+    }
+    
+}
